44eb583ead155e76e574550949654236f26128dd
sky5454 <sky5454@users.noreply.github.com>
2020-04-09 13:17:12 +0800

    fixed last change: 修复与用户角色表相关的操作（role_id: userinfo->user） now repaired!

--- src/main/java/com/demo/mapper/analysis/userAnalysis.java
+++ src/main/java/com/demo/mapper/analysis/userAnalysis.java
@@ -15,16 +15,16 @@ public interface userAnalysis {
             " group by role.role_name, year0" +
             " order by year0) a" +
             "     RIGHT JOIN (" +
-            "         SELECT distinct role_name, YEAR(ua.create_time) as year FROM user ua, role, userinfo WHERE role.id = userinfo.role_id" +
-            "         ) b" +
-            " ON a.role_name = b.role_name and year0=year" +
+            "            SELECT distinct role_name, YEAR(ua.create_time) as year FROM user ua, role, userinfo WHERE role.id = ua.role_id" +
+            "      ) b" +
+            "     ON a.role_name = b.role_name and year0=year" +
             "     order by year, role_name" )
     List<userNewAls> selectNewUser();
 
 
     @SQL("SELECT role.role_name, count(user.id) as count" +
-            "    FROM role, user, userinfo" +
-            "    WHERE userinfo.id = userinfo_id and userinfo.role_id = role.id" +
+            "    FROM role, user" +
+            "    WHERE user.role_id = role.id" +
             "    group by role.role_name")
     List<userPieAls> selectPercent();
 
